80e64bad0b4614739670ffd2583a6d87ea3cf8a3
Gaeus Caskie <fabrogaeuscaskie@gmail.com>
2022-05-21 21:06:13 +0800

    firstCommit
    First Commit for Assignment 2 :)

---
+++ ticTacToe/ticTacToe/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ticTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.designPanel = new System.Windows.Forms.Panel();
+            this.insertBox15 = new System.Windows.Forms.Button();
+            this.insertBox14 = new System.Windows.Forms.Button();
+            this.insertBox13 = new System.Windows.Forms.Button();
+            this.insertBox12 = new System.Windows.Forms.Button();
+            this.insertBox11 = new System.Windows.Forms.Button();
+            this.insertBox10 = new System.Windows.Forms.Button();
+            this.insertBox9 = new System.Windows.Forms.Button();
+            this.insertBox4 = new System.Windows.Forms.Button();
+            this.insertBox8 = new System.Windows.Forms.Button();
+            this.insertBox6 = new System.Windows.Forms.Button();
+            this.insertBox7 = new System.Windows.Forms.Button();
+            this.insertBox5 = new System.Windows.Forms.Button();
+            this.insertBox3 = new System.Windows.Forms.Button();
+            this.insertBox2 = new System.Windows.Forms.Button();
+            this.insertBox1 = new System.Windows.Forms.Button();
+            this.playAgainBtn = new System.Windows.Forms.CheckBox();
+            this.designPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // designPanel
+            // 
+            this.designPanel.Controls.Add(this.insertBox15);
+            this.designPanel.Controls.Add(this.insertBox14);
+            this.designPanel.Controls.Add(this.insertBox13);
+            this.designPanel.Controls.Add(this.insertBox12);
+            this.designPanel.Controls.Add(this.insertBox11);
+            this.designPanel.Controls.Add(this.insertBox10);
+            this.designPanel.Controls.Add(this.insertBox9);
+            this.designPanel.Controls.Add(this.insertBox4);
+            this.designPanel.Controls.Add(this.insertBox8);
+            this.designPanel.Controls.Add(this.insertBox6);
+            this.designPanel.Controls.Add(this.insertBox7);
+            this.designPanel.Controls.Add(this.insertBox5);
+            this.designPanel.Controls.Add(this.insertBox3);
+            this.designPanel.Controls.Add(this.insertBox2);
+            this.designPanel.Controls.Add(this.insertBox1);
+            this.designPanel.Location = new System.Drawing.Point(32, 12);
+            this.designPanel.Name = "designPanel";
+            this.designPanel.Size = new System.Drawing.Size(1237, 737);
+            this.designPanel.TabIndex = 0;
+            // 
+            // insertBox15
+            // 
+            this.insertBox15.Location = new System.Drawing.Point(988, 488);
+            this.insertBox15.Name = "insertBox15";
+            this.insertBox15.Size = new System.Drawing.Size(250, 250);
+            this.insertBox15.TabIndex = 14;
+            this.insertBox15.UseVisualStyleBackColor = true;
+            this.insertBox15.Click += new System.EventHandler(this.insertBox15_Click);
+            // 
+            // insertBox14
+            // 
+            this.insertBox14.Location = new System.Drawing.Point(741, 488);
+            this.insertBox14.Name = "insertBox14";
+            this.insertBox14.Size = new System.Drawing.Size(250, 250);
+            this.insertBox14.TabIndex = 13;
+            this.insertBox14.UseVisualStyleBackColor = true;
+            this.insertBox14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // insertBox13
+            // 
+            this.insertBox13.Location = new System.Drawing.Point(494, 488);
+            this.insertBox13.Name = "insertBox13";
+            this.insertBox13.Size = new System.Drawing.Size(250, 250);
+            this.insertBox13.TabIndex = 12;
+            this.insertBox13.UseVisualStyleBackColor = true;
+            this.insertBox13.Click += new System.EventHandler(this.insertBox13_Click);
+            // 
+            // insertBox12
+            // 
+            this.insertBox12.Location = new System.Drawing.Point(247, 488);
+            this.insertBox12.Name = "insertBox12";
+            this.insertBox12.Size = new System.Drawing.Size(250, 250);
+            this.insertBox12.TabIndex = 11;
+            this.insertBox12.UseVisualStyleBackColor = true;
+            this.insertBox12.Click += new System.EventHandler(this.insertBox12_Click);
+            // 
+            // insertBox11
+            // 
+            this.insertBox11.Location = new System.Drawing.Point(0, 488);
+            this.insertBox11.Name = "insertBox11";
+            this.insertBox11.Size = new System.Drawing.Size(250, 250);
+            this.insertBox11.TabIndex = 10;
+            this.insertBox11.UseVisualStyleBackColor = true;
+            this.insertBox11.Click += new System.EventHandler(this.insertBox11_Click);
+            // 
+            // insertBox10
+            // 
+            this.insertBox10.Location = new System.Drawing.Point(988, 247);
+            this.insertBox10.Name = "insertBox10";
+            this.insertBox10.Size = new System.Drawing.Size(250, 250);
+            this.insertBox10.TabIndex = 9;
+            this.insertBox10.UseVisualStyleBackColor = true;
+            this.insertBox10.Click += new System.EventHandler(this.insertBox10_Click);
+            // 
+            // insertBox9
+            // 
+            this.insertBox9.Location = new System.Drawing.Point(741, 247);
+            this.insertBox9.Name = "insertBox9";
+            this.insertBox9.Size = new System.Drawing.Size(250, 250);
+            this.insertBox9.TabIndex = 8;
+            this.insertBox9.UseVisualStyleBackColor = true;
+            this.insertBox9.Click += new System.EventHandler(this.insertBox9_Click);
+            // 
+            // insertBox4
+            // 
+            this.insertBox4.Location = new System.Drawing.Point(741, 0);
+            this.insertBox4.Name = "insertBox4";
+            this.insertBox4.Size = new System.Drawing.Size(250, 250);
+            this.insertBox4.TabIndex = 3;
+            this.insertBox4.UseVisualStyleBackColor = true;
+            this.insertBox4.Click += new System.EventHandler(this.insertBox4_Click);
+            // 
+            // insertBox8
+            // 
+            this.insertBox8.Location = new System.Drawing.Point(494, 247);
+            this.insertBox8.Name = "insertBox8";
+            this.insertBox8.Size = new System.Drawing.Size(250, 250);
+            this.insertBox8.TabIndex = 7;
+            this.insertBox8.UseVisualStyleBackColor = true;
+            this.insertBox8.Click += new System.EventHandler(this.insertBox8_Click);
+            // 
+            // insertBox6
+            // 
+            this.insertBox6.Location = new System.Drawing.Point(0, 247);
+            this.insertBox6.Name = "insertBox6";
+            this.insertBox6.Size = new System.Drawing.Size(250, 250);
+            this.insertBox6.TabIndex = 5;
+            this.insertBox6.UseVisualStyleBackColor = true;
+            this.insertBox6.Click += new System.EventHandler(this.insertBox6_Click);
+            // 
+            // insertBox7
+            // 
+            this.insertBox7.Location = new System.Drawing.Point(247, 247);
+            this.insertBox7.Name = "insertBox7";
+            this.insertBox7.Size = new System.Drawing.Size(250, 250);
+            this.insertBox7.TabIndex = 6;
+            this.insertBox7.UseVisualStyleBackColor = true;
+            this.insertBox7.Click += new System.EventHandler(this.insertBox7_Click);
+            // 
+            // insertBox5
+            // 
+            this.insertBox5.Location = new System.Drawing.Point(988, 0);
+            this.insertBox5.Name = "insertBox5";
+            this.insertBox5.Size = new System.Drawing.Size(250, 250);
+            this.insertBox5.TabIndex = 4;
+            this.insertBox5.UseVisualStyleBackColor = true;
+            this.insertBox5.Click += new System.EventHandler(this.insertBox5_Click);
+            // 
+            // insertBox3
+            // 
+            this.insertBox3.Location = new System.Drawing.Point(494, 0);
+            this.insertBox3.Name = "insertBox3";
+            this.insertBox3.Size = new System.Drawing.Size(250, 250);
+            this.insertBox3.TabIndex = 2;
+            this.insertBox3.UseVisualStyleBackColor = true;
+            this.insertBox3.Click += new System.EventHandler(this.insertBox3_Click);
+            // 
+            // insertBox2
+            // 
+            this.insertBox2.Location = new System.Drawing.Point(247, 0);
+            this.insertBox2.Name = "insertBox2";
+            this.insertBox2.Size = new System.Drawing.Size(250, 250);
+            this.insertBox2.TabIndex = 1;
+            this.insertBox2.UseVisualStyleBackColor = true;
+            this.insertBox2.Click += new System.EventHandler(this.insertBox2_Click);
+            // 
+            // insertBox1
+            // 
+            this.insertBox1.Location = new System.Drawing.Point(0, 0);
+            this.insertBox1.Name = "insertBox1";
+            this.insertBox1.Size = new System.Drawing.Size(250, 250);
+            this.insertBox1.TabIndex = 0;
+            this.insertBox1.UseVisualStyleBackColor = true;
+            this.insertBox1.Click += new System.EventHandler(this.insertBox1_Click);
+            // 
+            // playAgainBtn
+            // 
+            this.playAgainBtn.AutoSize = true;
+            this.playAgainBtn.Location = new System.Drawing.Point(1368, 277);
+            this.playAgainBtn.Name = "playAgainBtn";
+            this.playAgainBtn.Size = new System.Drawing.Size(82, 17);
+            this.playAgainBtn.TabIndex = 1;
+            this.playAgainBtn.Text = "Play Again?";
+            this.playAgainBtn.UseVisualStyleBackColor = true;
+            this.playAgainBtn.CheckedChanged += new System.EventHandler(this.playAgainBtn_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1545, 775);
+            this.Controls.Add(this.playAgainBtn);
+            this.Controls.Add(this.designPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.designPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel designPanel;
+        private System.Windows.Forms.Button insertBox1;
+        private System.Windows.Forms.Button insertBox6;
+        private System.Windows.Forms.Button insertBox5;
+        private System.Windows.Forms.Button insertBox4;
+        private System.Windows.Forms.Button insertBox3;
+        private System.Windows.Forms.Button insertBox2;
+        private System.Windows.Forms.Button insertBox8;
+        private System.Windows.Forms.Button insertBox7;
+        private System.Windows.Forms.Button insertBox10;
+        private System.Windows.Forms.Button insertBox9;
+        private System.Windows.Forms.Button insertBox15;
+        private System.Windows.Forms.Button insertBox14;
+        private System.Windows.Forms.Button insertBox13;
+        private System.Windows.Forms.Button insertBox12;
+        private System.Windows.Forms.Button insertBox11;
+        private System.Windows.Forms.CheckBox playAgainBtn;
+    }
+}
+
---
+++ ticTacToe/ticTacToe/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ticTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void playAgainBtn_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void insertBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void insertBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void insertBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertBox13_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void insertBox15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
